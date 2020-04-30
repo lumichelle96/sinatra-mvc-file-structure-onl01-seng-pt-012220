bc6982af5a61ae1ec56f55dca50bb7074d8e30da
Michelle Lu <lu.michelle96@icloud.com>
2020-04-30 22:49:48 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,15 +1,15 @@
 class Dog
   attr_accessor :name, :breed, :age
-  
+
   @@all = []
-  
-  def intialize(name,breed,age)
+
+  def initialize(name, breed, age)
     self.name = name
     self.breed = breed
     self.age = age
     self.class.all << self
   end
-  
+
   def self.all
     @@all
   end
